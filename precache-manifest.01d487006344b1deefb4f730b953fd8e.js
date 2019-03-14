b6c8aef57957689fb48bec90fbea05ac2e9a1103
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-03-15 00:51:05 +0700

    Updates

---
+++ precache-manifest.01d487006344b1deefb4f730b953fd8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0945be15e27dd30a16be",
+    "url": "/react-todo-app/static/js/runtime~main.0cc325c4.js"
+  },
+  {
+    "revision": "50fd3aa5de3519dd1301",
+    "url": "/react-todo-app/static/js/main.0fb88dcb.chunk.js"
+  },
+  {
+    "revision": "0cd72edf018b732efbeb",
+    "url": "/react-todo-app/static/js/2.5547fcf3.chunk.js"
+  },
+  {
+    "revision": "50fd3aa5de3519dd1301",
+    "url": "/react-todo-app/static/css/main.6f182375.chunk.css"
+  },
+  {
+    "revision": "0cd72edf018b732efbeb",
+    "url": "/react-todo-app/static/css/2.63642dbe.chunk.css"
+  },
+  {
+    "revision": "96ebd0e77e38b95437c4ced91f1301e8",
+    "url": "/react-todo-app/index.html"
+  }
+];
